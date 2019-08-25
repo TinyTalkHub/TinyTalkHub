d06df0a2b84cee15dab503e47a1138eadfa2a10c
unknown <holly@tinytalkhub.com>
2019-08-25 17:19:29 +0100

    Initial build

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
   <script src="https://kit.fontawesome.com/896fc5b83f.js" integrity="sha384-OAKDWjOpG0bQXRy41SslIR0heiiGZgnS0jmLIHzSouiddvpUQBcko17yKzA5R9SK" crossorigin="anonymous"></script>
 </head>
 <body>
-<i class="fas fa-cog"></i>
+  <div class="profile-bar">
+    <i class="fas fa-cog"></i>
+  </div>
 </body>
 </html>
