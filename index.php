aa11e3d74b16b03cdde32bda381b3e4fc20fcd5b
unknown <holly@tinytalkhub.com>
2019-08-25 18:29:29 +0100

    Initial build

--- index.php
+++ index.php
@@ -6,9 +6,9 @@
 </head>
 <body>
   <div class="profile-bar">
-    <div class="profile-bar-item profile-bar-cog"><i class="fas fa-cog"></i><span class"profile-bar-text"></span></div>
-    <div class="profile-bar-item profile-bar-gem"><i class="far fa-gem"></i><span class"profile-bar-text"> 1720 Gems</span></div>
-    <div class="profile-bar-item profile-bar-crown"><i class="fas fa-crown"></i><span class"profile-bar-text"> 300 Crowns</span></div>
+    <div class="profile-bar-item profile-bar-cog"><i class="fas fa-cog"></i><span class"profile-bar-text">Login</span></div>
+    <div class="profile-bar-item profile-bar-gem"><i class="far fa-gem"></i><span class"profile-bar-text"> 1720</span></div>
+    <div class="profile-bar-item profile-bar-crown"><i class="fas fa-crown"></i><span class"profile-bar-text"> 300</span></div>
   </div>
   <div class="type-bar">
     <div class="type-wrapper">
