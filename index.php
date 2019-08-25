86eb58e792c8a268d869e36d8cbc4eb3c210328e
unknown <holly@tinytalkhub.com>
2019-08-25 18:11:31 +0100

    Initial build

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
   <div class="type-bar">
     <div class="type-wrapper">
       <div class="type-box"><div class="type-box-icon"><i class="far fa-clone"></i></div><div class="type-box-title">Flashcards</div></div>
-      <div class="type-box"><div class="type-box-icon"><i class="far fa-clone"></i></div><div class="type-box-title"></div></div>
-      <div class="type-box"><div class="type-box-icon"><i class="far fa-clone"></i></div><div class="type-box-title"></div></div>
+      <div class="type-box"><div class="type-box-icon"><i class="far fa-pencil-alt"></i></div><div class="type-box-title">Practice</div></div>
+      <div class="type-box"><div class="type-box-icon"><i class="far fa-microphone"></i></div><div class="type-box-title">Pronunciation</div></div>
     </div>
   </div>
 </body>
