678cad705924a60d1b2e94f85cbcb62e7d2affd2
unknown <holly@tinytalkhub.com>
2019-08-25 17:06:53 +0100

    Initial build

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <html>
 <head>
   <link href="/theme/main.css" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/896fc5b83f.js" integrity="sha384-OAKDWjOpG0bQXRy41SslIR0heiiGZgnS0jmLIHzSouiddvpUQBcko17yKzA5R9SK" crossorigin="anonymous"></script>
 </head>
 <body>
-Nothing here.
+<i class="fas fa-cog"></i>
 </body>
 </html>
